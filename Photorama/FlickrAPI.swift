2a2873b3200a63e1b7aceb8d0a64a504c386acb1
Yemi Ajibola <ya9p8@mst.edu>
2016-09-11 15:47:45 -0500

    Added FlickrAPI class.

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,50 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Yemi Ajibola on 9/11/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case RecentPhotos = "flickr.photos.getRecent"
+}
+
+struct FlickrAPI {
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let APIKey = "20d79bd90621b5c56a0369f659852c8a"
+    
+    private static func flickrURL(method method: Method, parameters: [String: String]?) -> NSURL {
+        let components = NSURLComponents(string: baseURLString)!
+        
+        var queryItems = [NSURLQueryItem]()
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": APIKey
+        ]
+        
+        for (key, value) in baseParams {
+            let item = NSURLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+
+        
+        if let additionalParams = parameters {
+            for(key, value) in additionalParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        return components.URL!
+    }
+    
+    static func recentPhotosURL() -> NSURL {
+        return flickrURL(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+}
---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Yemi Ajibola on 9/11/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
